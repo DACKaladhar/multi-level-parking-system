05e286f5c860fcef8d2c8ed96b565a82d97049dd
DACKaladhar <darck21042004@gmail.com>
2024-11-27 19:10:32 +0530

    resolved the issues by replacing both ParkingSlotsDB and MaintenanceSlotsDB with PSDB

--- frontend/src/components/company-panel-renderer.tsx
+++ frontend/src/components/company-panel-renderer.tsx
@@ -2,59 +2,43 @@ import React from "react";
 import { MonitorPanelContainer } from "./monitor-panel-container";
 import { ConfigurationPanelContainer } from "./configuration-panel-container";
 import {
-  IMaintenanceSlot,
-  IParkingSlotsDB,
+  IBuildingDB,
   PanelMode,
 } from "./components-common-utils/common-parking-slot.interface";
 import { MaintenancePanelContainer } from "./maintenance-panel-container";
 
 interface ICompanyPanelRenderer {
-  parkingSlotsDB: IParkingSlotsDB[][];
-  writeIntoPSDB: (psdb: IParkingSlotsDB[][]) => void;
-  maintenanceSlotsDB: IMaintenanceSlot[][];
-  writeIntoMSDB: (msdb: IMaintenanceSlot[][]) => void;
+  PSDB: IBuildingDB[];
+  writeIntoPSDB: (psdb: IBuildingDB[]) => void;
   panelMode: PanelMode;
 }
 
 /**
  *
- * @param parkingSlotsDB - current state of parking facility
- * @param maintenanceSlotsDB - current state of parking facility slot properties
+ * @param PSDB - current state of parking facility
  * @param panelMode - selected panel mode by the user
  * @callback writeIntoPSDB - a callback function which writes the updated parking facility to PSDB
- * @callback writeIntoMSDB - a callback function which writes the updated maintenance slots database
  * @returns Conditionally renders the selected panel based on panelMode property
  */
 
 export const CompanyPanelRenderer: React.FC<ICompanyPanelRenderer> = ({
-  parkingSlotsDB,
+  PSDB,
   writeIntoPSDB,
-  maintenanceSlotsDB,
-  writeIntoMSDB,
   panelMode,
 }) => {
   return (
     <>
-      {panelMode === PanelMode.Monitor && (
-        <MonitorPanelContainer
-          parkingSlotsDB={parkingSlotsDB}
-          maintenanceSlotsDB={maintenanceSlotsDB}
-        />
-      )}
+      {panelMode === PanelMode.Monitor && <MonitorPanelContainer PSDB={PSDB} />}
 
       {panelMode === PanelMode.Configure && (
         <ConfigurationPanelContainer
-          parkingSlotsDB={parkingSlotsDB}
+          PSDB={PSDB}
           writeIntoPSDB={writeIntoPSDB}
         />
       )}
 
       {panelMode === PanelMode.Maintenance && (
-        <MaintenancePanelContainer
-          parkingSlotsDB={parkingSlotsDB}
-          maintenanceSlotsDB={maintenanceSlotsDB}
-          writeIntoMSDB={writeIntoMSDB}
-        />
+        <MaintenancePanelContainer PSDB={PSDB} writeIntoPSDB={writeIntoPSDB} />
       )}
     </>
   );
